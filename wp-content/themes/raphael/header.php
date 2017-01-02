ac1e83f44661d01ee78a5aca035636bc6e000731
Raphael Chang <raphaelchang1227@gmail.com>
2017-01-02 11:03:43 +0000

    New year, new website!

---
+++ wp-content/themes/raphael/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+    <title><?php
+        if ( is_single() ) { single_post_title(); }
+        elseif ( is_home() || is_front_page() ) { bloginfo('name'); get_page_number(); }
+        elseif ( is_page() ) { single_post_title('');  print ' | '; bloginfo('name');}
+        elseif ( is_search() ) { bloginfo('name'); print ' | Search results for ' . wp_specialchars($s); get_page_number(); }
+        elseif ( is_404() ) { bloginfo('name'); print ' | Not Found'; }
+        else { bloginfo('name'); wp_title('|'); get_page_number(); }
+    ?></title>
+ 
+    <meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+ 
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <link rel="stylesheet" id="foundation_icons_styles-css" href="<?php bloginfo('template_url'); ?>/foundation-icons/foundation-icons.css?ver=4.2.5" type="text/css" media="all">
+ 
+    <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
+
+    <?php wp_head(); ?>
+ 
+    <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" title="<?php printf( __( '%s latest posts', 'hbd-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+    <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php printf( __( '%s latest comments', 'hbd-theme' ), wp_specialchars( get_bloginfo('name'), 1 ) ); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+</head>
+<body>
+<div id="wrapper" class="hfeed">
+    <div id="header">
+        <div id="masthead">
+        <div id="title"><h1><a href="<?php home_url() ?>"><?php bloginfo('name') ?></a></h1></div>
+<div id="tagline"><h3><?php bloginfo('description'); ?><h3></div>
+<div style="clear:both"></div>
+<hr>
+            <div id="access">
+				<!--<div class="skip-link"><a href="#content" title="<?php _e( 'Skip to content', 'hbd-theme' ) ?>"><?php _e( 'Skip to content', 'hbd-theme' ) ?></a></div>-->
+				<?php #wp_page_menu( 'sort_column=menu_order' ); ?>
+				<?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-header', 'depth' => '1' ) ); ?>
+            </div><!-- #access -->
+        </div><!-- #masthead -->
+<div style="background-color: #f6fcff; height: 8px"></div>
+    </div><!-- #header -->
+ 
+    <div id="main">
---
+++ wp-content/themes/raphael2017/js/script.js
@@ -0,0 +1,220 @@
+$( document ).ready(function()
+        {
+            $('.grid').masonry({
+                itemSelector: '.project_entry',
+                percentPosition: true
+            });
+            fontsize = function () {
+                    var margin = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue("--entry-margin"), 10);
+                    var gridWidth = $(".grid").innerWidth();
+                    var numColumns = Math.floor(gridWidth / (230 + 2 * margin));
+                    var colPercentage = 1 / numColumns * 100;
+                    document.documentElement.style.setProperty("--entry-size", "calc(" + colPercentage + "% - var(--entry-margin) * 2)");
+                    document.documentElement.style.setProperty("--entry-size-double", "calc(" + colPercentage * 2 + "% - var(--entry-margin) * 2)");
+                    var width = $(".project_entry").not(".grid_width2, .grid_height2").width();
+                    var fontSize = Math.min(17.5, width * 0.07);
+                    var fontSizeSmaller = Math.min(16, width * 0.065);
+                    var fontSizeText = Math.min(13.5, width * 0.057);
+                    $(".project_entry .caption").css('font-size', fontSizeText);
+                    $(".project_entry .caption h4").not(".smaller").css('font-size', fontSize);
+                    $(".project_entry .caption h4.smaller").css('font-size', fontSizeSmaller);
+            };
+            fontsize();
+            $(window).resize(fontsize);
+            var pageOpen = false;
+            var scrolling = false;
+            var closePage = function() {
+                pageOpen = false;
+                $('#page-content').animate({top: '10%', opacity: 0}, 200);
+                $('#project-page').addClass('hidescrollbar');
+                $('html').removeClass('hidescrollbar');
+                $('#close').css('right', '');
+                $('#close').removeClass('fixed');
+                $('#header').css('right', '');
+                $('#project-page').animate({top: '100vh'}, 300, function(){
+                    $('#loading').css('display', 'none');
+                    $('#page-content').text('');
+                });
+            };
+            var openHandler = function() {
+                event.preventDefault();
+                var id = $(this).find("a").attr("href").split("/")[1];
+                history.pushState({}, '', '#projects/' + id);
+                openPage(id);
+            };
+            var closeHandler = function() {
+                history.pushState({}, '', '#');
+                closePage();
+            };
+            var openPage = function(page) {
+                if (pageOpen)
+                    return;
+                $('#project-page').removeClass('hidescrollbar');
+                $('#project-page').animate({top: 0}, 300, function() {
+                    $('#close').addClass('fixed');
+                });
+                $('#page-content').text('');
+                $('#loading').css('display', 'block');
+                pageOpen = true;
+                $.getJSON("wp-json/wp/v2/pages?slug=" + page, function( data ) {
+                    if (pageOpen)
+                    {
+                        var scrollbarWidth = window.innerWidth - $('#project-page').innerWidth();
+                        $('#close').css('right', 25 + scrollbarWidth);
+                        $('#header').css('right', scrollbarWidth);
+                        $('html').addClass('hidescrollbar');
+                        $('#loading').css('display', 'none');
+                        $('#page-content').text('');
+                        $('#page-content').append('<h1>' + data[0].title.rendered + '</h1>');
+                        $('#page-content').append(data[0].content.rendered);
+                        $('#page-content').animate({top: 0, opacity: 1.0}, 200);
+                    }
+                });
+            };
+            var swapPage = function(page) {
+                if (!pageOpen)
+                    return;
+                $('#page-content').animate({top: '10%', opacity: 0}, 200, function() {
+                    $('#loading').css('display', 'none');
+                    $('#page-content').text('');
+                    $('#loading').css('display', 'block');
+                    pageOpen = true;
+                    $.getJSON("wp-json/wp/v2/pages?slug=" + page, function( data ) {
+                        if (pageOpen)
+                        {
+                            var scrollbarWidth = window.innerWidth - $('#project-page').innerWidth();
+                            $('#header').css('right', scrollbarWidth);
+                            $('#close').css('right', 25 + scrollbarWidth);
+                            $('html').addClass('hidescrollbar');
+                            $('#loading').css('display', 'none');
+                            $('#page-content').text('');
+                            $('#page-content').append('<h1>' + data[0].title.rendered + '</h1>');
+                            $('#page-content').append(data[0].content.rendered);
+                            $('#page-content').animate({top: 0, opacity: 1.0}, 200);
+                        }
+                    });
+                });
+            };
+            if(window.location.hash) {
+                var list = window.location.hash.split("/");
+                hash = list[0];
+                var offset = 0;
+                if (hash == '#projects')
+                {
+                    offset = $('#header').outerHeight();
+                }
+                $('html, body').scrollTop($(hash).offset().top - offset);
+                if (hash == "#projects" && list.length >= 2 && list[1] != '')
+                {
+                    openPage(list[1]);
+                }
+            }
+            $(window).bind('hashchange', function (event) {
+                event.preventDefault();
+                var list = window.location.hash.split("/");
+                hash = list[0];
+                if (hash == "#projects" && list.length >= 2 && list[1] != '')
+                {
+                    if (!pageOpen)
+                        openPage(list[1]);
+                    else
+                    {
+                        swapPage(list[1]);
+                    }
+                }
+                else if (pageOpen)
+                {
+                    closePage();
+                }
+                else
+                {
+                    var offset = 0;
+                    if (hash == '#projects')
+                    {
+                        offset = $('#header').outerHeight();
+                    }
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top - offset
+                    }, 500);
+                }
+            });
+            var findMiddleElement = (function(docElm){
+                var viewportHeight = docElm.clientHeight,
+                elements = $('.home_box, .projects_container, .contact_box'); 
+
+                return function(e){
+                    if (scrolling)
+                        return;
+                    var middleElement;
+                    if (e && e.type == 'resize')
+                        viewportHeight = docElm.clientHeight;
+
+                    elements.each(function(){
+                        var box_top = this.getBoundingClientRect().top;
+                        var box_bot = this.getBoundingClientRect().bottom;
+                        if( box_bot > viewportHeight / 2 && box_top < viewportHeight / 2){
+                            middleElement = this;
+                            return false;
+                        }
+                    });
+                    if ($(middleElement).hasClass('home_box'))
+                    {
+                        $('#menu-home').addClass('current_page_item');
+                        $('#menu-projects').removeClass('current_page_item');
+                        $('#menu-contact').removeClass('current_page_item');
+                    }
+                    else if ($(middleElement).hasClass('projects_container'))
+                    {
+                        $('#menu-home').removeClass('current_page_item');
+                        $('#menu-projects').addClass('current_page_item');
+                        $('#menu-contact').removeClass('current_page_item');
+                    }
+                    else if ($(middleElement).hasClass('contact_box'))
+                    {
+                        $('#menu-home').removeClass('current_page_item');
+                        $('#menu-projects').removeClass('current_page_item');
+                        $('#menu-contact').addClass('current_page_item');
+                    }
+                }
+            })(document.documentElement);
+
+            $(window).on('scroll resize', findMiddleElement);
+            findMiddleElement();
+            $('.project_entry').on('click', openHandler);
+            $('#close').on('click', closeHandler);
+            $('#masthead').css('border-color', 'rgba(136, 136, 136, ' + Math.max(0, Math.min(1, 1.0 - ($(window).scrollTop() - 10.0) / 30.0)) + ')');
+            $('#header').css('background', 'rgba(0, 0, 0, ' + Math.max(0, Math.min(0.88, ($(window).scrollTop() - 60.0) / 50.0)) + ')');
+            $(window).scroll(function() {
+                if (!pageOpen)
+                {
+                    $('#masthead').css('border-color', 'rgba(136, 136, 136, ' + Math.max(0, Math.min(1, 1.0 - ($(window).scrollTop() - 10.0) / 30.0)) + ')');
+                    $('#header').css('background', 'rgba(0, 0, 0, ' + Math.max(0, Math.min(0.88, ($(window).scrollTop() - 60.0) / 50.0)) + ')');
+                }
+                else
+                {
+                    $('#masthead').css('border-color', 'rgba(136, 136, 136, 0)');
+                    $('#header').css('background', 'rgba(0, 0, 0, 0.88)');
+                }
+            });
+            $(document).on('click', 'a.menu-item', function(event){
+                event.preventDefault();
+                if (pageOpen)
+                {
+                    closePage();
+                }
+                var id = $.attr(this, 'href');
+                history.pushState({}, '', id);
+                var offset = 0;
+                if (id == '#projects')
+                {
+                    offset = $('#header').outerHeight();
+                }
+                scrolling = true;
+                $('html, body').animate({
+                    scrollTop: $(id).offset().top - offset
+                }, 500, function(){
+                    scrolling = false;
+                findMiddleElement();
+                });
+            });
+        });
